0b71b8f01cc3ccf0ad05c9646f2e683e40400d44
Nemirich Ivan <bebded@gmail.com>
2023-01-22 15:51:45 +0400

    upd plagins + content-types

--- src/api/meme/routes/meme.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * meme router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::meme.meme');
--- src/api/meme/services/meme.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * meme service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::meme.meme');
